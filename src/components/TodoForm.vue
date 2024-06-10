10f3a7c2cde31e3150258ab2e83d59b2eb23a826
black-code <legrandmahamat1@gmail.com>
2024-06-10 10:13:08 +0100

    mutating state with actions

--- src/components/TodoForm.vue
+++ src/components/TodoForm.vue
@@ -1,9 +1,27 @@
 <script setup>
 
+import {ref} from "vue";
+import {useTodoListStore} from "@/stores/todoList.js";
+
+const todo = ref('')
+const store = useTodoListStore()
+
+function addItemAndClear(item) {
+  if (item.length === 0) {
+    return
+  }
+  store.addTodo(item)
+  todo.value = ''
+}
 </script>
 
 <template>
-
+  <div>
+    <form @submit.prevent="addItemAndClear(todo)">
+      <input v-model="todo" type="text" />
+      <button>Add</button>
+    </form>
+  </div>
 </template>
 
 <style scoped>
--- src/stores/todoList.js
+++ src/stores/todoList.js
@@ -3,8 +3,20 @@ import { defineStore } from "pinia";
 export const useTodoListStore = defineStore("todoList", {
     // state
     state: () => ({
-        todoList: []
-    })
+        todoList: [],
+        id: 0
+    }),
     // getters
     // actions
+    actions: {
+        addTodo(item) {
+            this.todoList.push({item, id: this.id++, completed: false});
+        },
+        deleteTodo(itemId) {
+            this.todoList = this.todoList.filter((object) => {
+                return object.id !== itemId
+            })
+        }
+    }
+
 })
